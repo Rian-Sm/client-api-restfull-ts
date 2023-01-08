160b5b1b6383ca3d5201fd713c8bfb636b93e683
Rian Macedo <riansm100@gmail.com>
2023-01-07 22:07:06 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("ola");
+import express from 'express'
+import {config} from 'dotenv'
+
+config();
+
+const app = express()
+
+const port = process.env.PORT || 4000
+const host = process.env.HOST || 'http://localhost'
+
+app.get('/', (req, res)=>{
+    res.send("TESTE");
+})
+
+app.listen(port , ()=> console.log(`local url: ${host}:${port}`))
